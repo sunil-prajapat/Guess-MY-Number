bad0da2b5c0a25aaeaff322d9f568f0e1c6161c8
sunil-prajapat <sunilpra999@gmail.com>
2026-01-20 15:49:02 +0530

    add some functionality

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-'use strict';
+// 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector(".message").textContent = "Coreect Number!";
+// document.querySelector(".message").textContent = "Coreect Number!";
 
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 34;
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 34;
 
-document.querySelector(".guess").value = 45;
+// document.querySelector(".guess").value = 45;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High!';
+    score--;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low!';
+  }
+});
